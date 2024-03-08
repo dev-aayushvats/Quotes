a0d715d78e02fbbc256fe6a4b872ff6984a421d2
Max <124528608+dev-aayushvats@users.noreply.github.com>
2024-03-08 05:36:54 +0530

    Entire thig in one commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const api = "https://api.quotable.io/quotes/random?maxLength=50";
+const quotebox = document.querySelector("blockquote");
+const auth = document.getElementById("auth");
+
+const getQuote = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data);
+    quotebox.innerHTML = data[0].content;
+    auth.innerHTML = data[0].author;
+}
+getQuote(api);
